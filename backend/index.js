a536fa13ca9e9b473e7ee1f6f136e5bc85088a6d
UtkarshSuman <utkarshsuman7@gmail.com>
2026-03-07 20:49:24 +0530

    cors update

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://jokesapp-red.vercel.app"
+      "https://https://jokesapp26.vercel.app"
     ],
     credentials: true,
   })
